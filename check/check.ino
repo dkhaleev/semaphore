72faaf1f786c5c962e45a4e292a503c15d417eb3
Dmitry Khaleev <dkhaleev@gmail.com>
2018-03-19 21:41:29 +0200

    added simple check file for witty-cloud board

---
+++ check/check.ino
@@ -0,0 +1,40 @@
+//esp8266 simple check sketch
+#include <Streaming.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  Serial << "setup\n";
+}
+
+void blink(int pin) {
+  Serial << "analogRead(A0)=" << analogRead(A0) << "\n";
+
+  pinMode(pin, OUTPUT);
+  Serial << "pinMode(" << pin << ",OUTPUT)\n";
+  delay(1000);
+
+  digitalWrite(pin, 0);
+  Serial << "digitalWrite(" << pin << ",0)\n";
+  delay(1000);
+
+  digitalWrite(pin, 1);
+  Serial << "digitalWrite(" << pin << ",1)\n";
+  delay(1000);
+
+  for (int pct = 10; pct <= 90; pct += 10) {
+    analogWrite(pin, static_cast<int>(PWMRANGE * 0.01 * pct));
+    Serial << "analogWrite(" << pin << "," << pct << "%)\n";
+    delay(1000);
+  }
+
+  analogWrite(pin, 0);
+}
+
+void loop() {
+  blink(12); //
+  blink(13);
+  blink(15);
+}
