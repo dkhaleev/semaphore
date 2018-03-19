62baac18d24af37da71b51d9367a26afb2a5b60c
Dmitry Khaleev <dkhaleev@gmail.com>
2018-03-19 22:06:09 +0200

    added credentials sample file

---
+++ call-informer/credentials.sample.c
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char* host = "ESP-8266 semaphore";
+const char* ssid = "**********";
+const char* password = "**********";
+
+#endif //__CONFIGURATION_H
