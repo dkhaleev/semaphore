d0edd887e0ebd8beca49d527a688091482a2d6d7
Dmitry Khaleev <dkhaleev@gmail.com>
2018-03-21 00:36:39 +0200

    introduced HashMap to store GPIO pins state

--- call-informer/call-informer.ino
+++ call-informer/call-informer.ino
@@ -2,6 +2,7 @@
  * ESP8266 
 */
 #include "credentials.h"
+#include <HashMap.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -11,29 +12,44 @@
 uint8_t gpio1 = 12;
 bool    gpio1On = false;
 
+//HashMap storage for GPIOs and states
+const byte HASH_SIZE = 3; //number of outputs, sattelites
+// initialize storage
+HashType<int, bool> hashRawArray[HASH_SIZE];
+//handle storage
+HashMap<int, bool> hashMap = HashMap<int,bool>( hashRawArray , HASH_SIZE );
+
 //Init web-server
 ESP8266WebServer server(80);
 
 void setup()
 {
+  //assign hashmap [index](pin, state value)
+  hashMap[0](12, false); //green on witty-cloud
+  hashMap[1](13, false); //blue ~
+  hashMap[2](15, false); //red
+  
   //start serial for debug
   Serial.begin(115200);
   Serial.printf("\n\nFree memory %d\n", ESP.getFreeHeap());
   
   //Init output
-  pinMode(gpio1, OUTPUT);
-  digitalWrite(gpio1, gpio1On);
+  for(int index=0; index <=HASH_SIZE; index++)
+  {
+    pinMode(hashMap[index].getHash(), OUTPUT);
+    digitalWrite(hashMap[index].getHash(), hashMap[index].getValue());
+  }
 
-  //Connect WiFi
+  //Connect to WiFi
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
+  
   if(WiFi.waitForConnectResult() == WL_CONNECTED) {
     //Run Web-server
     MDNS.begin(host);
     server.on("/", HTTP_handleRoot);
     server.onNotFound(HTTP_handleRoot);
     server.begin();
-//    Serial.printf("\n\n Device enabled on %s\n", WiFi.localIP());
     Serial.println(WiFi.localIP());
   } else {
     Serial.printf("WiFi device init failure");
