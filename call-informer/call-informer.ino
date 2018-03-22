9bbb6dbb0739c656d86f9a338bf9b102b2b96785
Dmitry Khaleev <dkhaleev@gmail.com>
2018-03-23 00:52:40 +0200

    display statuses value and reworked hashMap processing

--- call-informer/call-informer.ino
+++ call-informer/call-informer.ino
@@ -1,5 +1,5 @@
 /**
- * ESP8266 WiFi Controlled semaphore
+   ESP8266 WiFi Controlled semaphore
 */
 #include "credentials.h"
 #include <HashMap.h>
@@ -8,16 +8,13 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-//Output GPIO
-uint8_t gpio1 = 12;
-bool    gpio1On = false;
-
 //HashMap storage for GPIOs and states
 const byte HASH_SIZE = 3; //number of outputs, sattelites
 // initialize storage
 HashType<int, bool> hashRawArray[HASH_SIZE];
 //handle storage
-HashMap<int, bool> hashMap = HashMap<int,bool>( hashRawArray , HASH_SIZE );
+HashMap<int, bool> hashMap = HashMap<int, bool>( hashRawArray , HASH_SIZE );
+
 
 //Init web-server
 ESP8266WebServer server(80);
@@ -32,9 +29,9 @@ void setup()
   //start serial for debug
   Serial.begin(115200);
   Serial.printf("\n\nFree memory %d\n", ESP.getFreeHeap());
-  
+
   //Init output
-  for(int index=0; index <=HASH_SIZE; index++)
+  for (int index = 0; index < HASH_SIZE; index++)
   {
     pinMode(hashMap[index].getHash(), OUTPUT);
     digitalWrite(hashMap[index].getHash(), hashMap[index].getValue());
@@ -43,8 +40,8 @@ void setup()
   //Connect to WiFi
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
-  
-  if(WiFi.waitForConnectResult() == WL_CONNECTED) {
+
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
     //Run Web-server
     MDNS.begin(host);
     server.on("/", HTTP_handleRoot);
@@ -61,18 +58,35 @@ void loop()
   server.handleClient();
   delay(50);
 }
-void HTTP_handleRoot(void) { 
+void HTTP_handleRoot(void) {
+  //local reflection of statuses
+  bool statuses[HASH_SIZE];
+  for (int index = 0; index < HASH_SIZE; index++)
+  {
+    statuses[index] = hashMap[index].getValue();
+  }
+
   String out = "";
   out =
-"<html>\
+    "<html>\
   <head>\
     <meta charset=\"utf-8\" />\
     <title>WiFi semaphore</title>\
   </head>\
   <body>\
-    <h1>WiFi Semaphore</h1>\
-  </body>\
+    <h1>WiFi Semaphore</h1>";
+      for (int i = 0; i < HASH_SIZE; i++)
+      {
+        bool status = statuses[i];
+        if (status)
+        {
+          out += "<span>true</span><br/>";
+        } else {
+          out += "<span>false</span><br/>";
+        }
+      }
+  out += "</body>\
 </html>";
-   server.send ( 200, "text/html", out );   
+  server.send ( 200, "text/html", out );
 }
 
